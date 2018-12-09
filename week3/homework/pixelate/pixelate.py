1e4bc1066109387a11728354af3556ecdc8a655a
shodai <k.shodai.0011@gmail.com>
2018-12-09 20:41:27 +0000

    change naming

--- week3/homework/pixelate/pixelate.py
+++ week3/homework/pixelate/pixelate.py
@@ -9,5 +9,5 @@ width = img.width
 with img.clone() as resize:
     resize.resize(int(width*0.05), int(height*0.05), 'box', 1)
     resize.resize(width, height, 'box', 1)
-    resize.save(filename='resize.jpg')
+    resize.save(filename='pixalated.jpg')
     print("Completed.")
